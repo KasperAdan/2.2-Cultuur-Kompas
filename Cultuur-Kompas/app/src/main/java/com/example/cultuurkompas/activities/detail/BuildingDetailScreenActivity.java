f70c0d95ef27584d75fb717c6429b0947b3fef06
Dave-Avans <dl.visser@student.avans.nl>
2020-12-07 17:12:42 +0100

    [Cleanup]: Fixed a lot of png problems and naming conventions. style aswell.

---
+++ Cultuur-Kompas/app/src/main/java/com/example/cultuurkompas/activities/detail/BuildingDetailScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.cultuurkompas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuildingDetailScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_building_detail_screen);
+    }
+}
---
+++ Cultuur-Kompas/app/src/main/java/com/example/cultuurkompas/activities/detail/RouteDetailScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.cultuurkompas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RouteDetailScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_detail_screen);
+    }
+}
