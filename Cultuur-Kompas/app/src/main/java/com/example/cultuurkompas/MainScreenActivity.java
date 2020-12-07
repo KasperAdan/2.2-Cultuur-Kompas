2720f643b8bc7c3fa46f5335b7381658a7db9e83
Dave-Avans <dl.visser@student.avans.nl>
2020-12-07 11:13:26 +0100

    [Commit]: Made the changes for the MapScreen

---
+++ Cultuur-Kompas/app/src/main/java/com/example/cultuurkompas/MainScreenActivity.java
@@ -0,0 +1,32 @@
+package com.example.cultuurkompas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainscreen);
+    }
+
+    public void onButtonCurrentLocationClick(View view){
+        Toast.makeText(this,"CURRENT LOCATION", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButtonHelpMapClick(View view){
+        Toast.makeText(this,"HELP!", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButtonBuildingMapClick(View view){
+        Toast.makeText(this,"BUILDINGS", Toast.LENGTH_LONG).show();
+    }
+
+    public void onButtonRouteMapClick(View view){
+        Toast.makeText(this,"ROUTES", Toast.LENGTH_LONG).show();
+    }
+}
